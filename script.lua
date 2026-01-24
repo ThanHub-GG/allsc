f477ba9ce75ebd3284c0c6938b551cfa0894308e
ThanHub-GG <oktarenata242@gmail.com>
2026-01-25 00:36:53 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -9,30 +9,6 @@ local games = {
         free = "https://api.luarmor.net/files/v4/loaders/e1f616d597f9d63c14cf649d711abac9.lua",
         premium = "https://api.luarmor.net/files/v4/loaders/6a543b585b21918489b0b3d5fe53bdb4.lua",
     }, -- Fisch
-    [4372130] = {
-        free = "https://raw.githubusercontent.com/ThanHub-GG/ThanKaitunBF/refs/heads/main/loader",
-        premium = "https://raw.githubusercontent.com/ThanHub-GG/ThanKaitunBF/refs/heads/main/prem.lua",
-    }, -- BF KAITUN
-    [35497641] = {
-        free = "https://raw.githubusercontent.com/ThanHub-GG/Beaks-Free/refs/heads/main/load",
-        premium = "http://raw.githubusercontent.com/ThanHub-GG/Beaks-Series/refs/heads/main/loader",
-    }, -- BEAKS
-    [11867394] = {
-        free = "https://raw.githubusercontent.com/ThanHub-GG/DeadRail/refs/heads/main/free",
-        premium = "https://raw.githubusercontent.com/ThanHub-GG/DeadRail/refs/heads/main/premium",
-    }, -- Dead Rails
-    [35789249] = {
-        free = "http://raw.githubusercontent.com/ThanHub-GG/GrowGarden/refs/heads/main/beta",
-        premium = "http://raw.githubusercontent.com/ThanHub-GG/GrowGarden/refs/heads/main/prembeta",
-    }, -- GrowGarden
-    [35289532] = {
-        free = "https://raw.githubusercontent.com/ThanHub-GG/DIG/refs/heads/main/digfree",
-        premium = "https://raw.githubusercontent.com/ThanHub-GG/DIG/refs/heads/main/digfisch.lua",
-    }, -- DIG
-    [35815907] = {
-        free = "",
-        premium = "https://raw.githubusercontent.com/ThanHub-GG/SAB/refs/heads/main/brainprem.lua",
-    }, -- SAB
     [6042520] = {
         free = "https://raw.githubusercontent.com/ThanHub-GG/99Forest/refs/heads/main/free.lua",
         premium = "https://raw.githubusercontent.com/ThanHub-GG/99Forest/refs/heads/main/prem.lua",
@@ -57,6 +33,30 @@ local games = {
         free = "",
         premium = "https://api.thanhub.xyz/uploads/ThanHubForgePremium.lua",
     }, -- FORGE
+    [35789249] = {
+        free = "http://raw.githubusercontent.com/ThanHub-GG/GrowGarden/refs/heads/main/beta",
+        premium = "http://raw.githubusercontent.com/ThanHub-GG/GrowGarden/refs/heads/main/prembeta",
+    }, -- GrowGarden
+    [4372130] = {
+        free = "https://raw.githubusercontent.com/ThanHub-GG/ThanKaitunBF/refs/heads/main/loader",
+        premium = "https://raw.githubusercontent.com/ThanHub-GG/ThanKaitunBF/refs/heads/main/prem.lua",
+    }, -- BF KAITUN
+    -- [35497641] = {
+    --     free = "https://raw.githubusercontent.com/ThanHub-GG/Beaks-Free/refs/heads/main/load",
+    --     premium = "http://raw.githubusercontent.com/ThanHub-GG/Beaks-Series/refs/heads/main/loader",
+    -- }, -- BEAKS
+    -- [11867394] = {
+    --     free = "https://raw.githubusercontent.com/ThanHub-GG/DeadRail/refs/heads/main/free",
+    --     premium = "https://raw.githubusercontent.com/ThanHub-GG/DeadRail/refs/heads/main/premium",
+    -- }, -- Dead Rails
+    -- [35289532] = {
+    --     free = "https://raw.githubusercontent.com/ThanHub-GG/DIG/refs/heads/main/digfree",
+    --     premium = "https://raw.githubusercontent.com/ThanHub-GG/DIG/refs/heads/main/digfisch.lua",
+    -- }, -- DIG
+    -- [35815907] = {
+    --     free = "",
+    --     premium = "https://raw.githubusercontent.com/ThanHub-GG/SAB/refs/heads/main/brainprem.lua",
+    -- }, -- SAB
 }
 
 local function executeScript(script_key)
